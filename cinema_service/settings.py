93f730a25c3b83b828b916d81167adfd8e7f1bcc
Oleksandr Mazur <a9p9m9@gmail.com>
2025-06-12 22:02:29 +0300

    turn off debug_toolbar

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "cinema",
     "user",
-    "debug_toolbar",
+    # "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -134,6 +134,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+# INTERNAL_IPS = [
+#     "127.0.0.1",
+# ]
